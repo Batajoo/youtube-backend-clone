b8fd6507d171b37bd5cc3f1da42bec1d8a486ef8
sanjiv <sanjivbatajoo@gmail.com>
2025-08-05 18:35:54 -0500

    subscription model and controller complete

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -62,4 +62,158 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         );
 });
 
-export {toggleSubscription};
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const userId = req.user?._id;
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Error: Invalid channel Id");
+    }
+
+    const userObjectId = new mongoose.Types.ObjectId(userId);
+
+    const channelListDetails = await Subscription.aggregate([
+        {
+            $match: {
+                channel: userObjectId,
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                let: {subscriberId: "$subscriber"},
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ["$_id", "$$subscriberId"],
+                            },
+                        },
+                    },
+                    {
+                        $project: {
+                            username: 1,
+                            email: 1,
+                            fullName: 1,
+                            avatar: 1,
+                            coverImage: 1,
+                        },
+                    },
+                ],
+                as: "subscriberInfo",
+            },
+        },
+        {
+            $unwind: "$subscriberInfo",
+        },
+        {
+            $group: {
+                _id: "$channel",
+                subscriberList: {$push: "$subscriberInfo"},
+                subscriberCount: {$sum: 1},
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                channel: "$_id",
+                subscriberCount: 1,
+                subscriberList: 1,
+            },
+        },
+    ]);
+
+    if (!channelListDetails) {
+        throw new ApiError(400, "Error fetching channel list");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                channelListDetails,
+                "Successful fetching channel list"
+            )
+        );
+});
+
+const getSubscribedChannel = asyncHandler(async (req, res) => {
+    const userChannelId = req.user?._id;
+
+    if (!isValidObjectId(userChannelId)) {
+        throw new ApiError(400, "Invalid user channel Id");
+    }
+
+    const userChannelObjectId = new mongoose.Types.ObjectId(userChannelId);
+
+    const subscribedChannels = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: userChannelObjectId,
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                let: {subscribedId: "$channel"},
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ["$_id", "$$subscribedId"],
+                            },
+                        },
+                    },
+                    {
+                        $project: {
+                            username: 1,
+                            email: 1,
+                            avatar: 1,
+                            coverImage: 1,
+                            fullName: 1,
+                        },
+                    },
+                ],
+                as: "subscribedToInfo",
+            },
+        },
+        {
+            $unwind: "$subscribedToInfo",
+        },
+        {
+            $group: {
+                _id: "$subscriber",
+                subscribedToList: {
+                    $push: "$subscribedToInfo",
+                },
+                subscribedToCount: {
+                    $sum: 1,
+                },
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                channel: "$_id",
+                subscribedToList: 1,
+                subscribedToCount: 1,
+            },
+        },
+    ]);
+
+    if (!subscribedChannels) {
+        throw new ApiError(400, "Error fetching subscribed channels");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                subscribedChannels,
+                "Successful fetching subscribed channels"
+            )
+        );
+});
+
+export {toggleSubscription, getUserChannelSubscribers, getSubscribedChannel};
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,10 +1,16 @@
 import {Router} from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
-import {toggleSubscription} from "../controllers/subscription.controller.js";
+import {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannel,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/toggle/:channelId").post(toggleSubscription);
+router.route("/subscribers").get(getUserChannelSubscribers);
+router.route("/subscribed").get(getSubscribedChannel);
 
 export default router;
