40a90f982a8fdb5e9405774c691cac57f3cbde81
sanjiv <sanjivbatajoo@gmail.com>
2025-08-09 18:04:03 -0500

    complete comment controller and model

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -6,11 +6,121 @@ import mongoose from "mongoose";
 
 const addComment = asyncHandler(async (req, res) => {
     const {videoId} = req.params;
-    const {commentContent} = req.body;
+    const {content} = req.body;
 
     if (!mongoose.isValidObjectId(videoId)) {
         throw new ApiError(400, "Invalid video Id");
     }
+
+    if (!req.user?._id) {
+        throw new ApiError(400, "UnAuthorized: Not logged in");
+    }
+
+    const comment = await Comment.create({
+        video: videoId,
+        content,
+        owner: req.user.id,
+    });
+
+    if (!comment) {
+        throw new ApiError(400, "Error: Unable to create comment");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "Comment added successfully"));
 });
 
-export {addComment};
+const updateComment = asyncHandler(async (req, res) => {
+    const {commentId} = req.params;
+    const {content} = req.body;
+
+    if (!content) {
+        throw new ApiError(400, "Error: Required Field/s missing");
+    }
+
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Error: Comment ID not valid");
+    }
+
+    const commentUser = await Comment.findById(commentId);
+
+    if (!commentUser?.owner.equals(req.user?._id)) {
+        throw new ApiError(
+            400,
+            "Unauthorized: User not the owner of this comment"
+        );
+    }
+
+    const comment = await Comment.findByIdAndUpdate(commentId, {
+        $set: {
+            content: content,
+        },
+    });
+
+    if (!comment) {
+        throw new ApiError(400, "Error: Comment not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "Suceesfully updated comment"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+    const {commentId} = req.params;
+
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(200, "Invalid: comment id invalid");
+    }
+
+    const comment = await Comment.findByIdAndDelete(commentId);
+
+    if (!comment) {
+        throw new ApiError(400, "Error: Comment not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "Successfully deleted comment"));
+});
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    const {videoId} = req.params;
+    const {page = 1, limit = 10} = req.query;
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(200, "Invalid: video id invalid");
+    }
+
+    const aggregate = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId),
+            },
+        },
+    ]);
+
+    const options = {
+        page,
+        limit,
+    };
+
+    const comments = await Comment.aggregatePaginate(aggregate, options);
+
+    if (!comments) {
+        throw new ApiError(400, "Error: Video not found");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                comments,
+                "Successfully fetched video comments"
+            )
+        );
+});
+
+export {addComment, updateComment, deleteComment, getVideoComments};
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,7 +1,16 @@
 import {Router} from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
+import {
+    addComment,
+    updateComment,
+    deleteComment,
+    getVideoComments,
+} from "../controllers/comment.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
+
+router.route("/:videoId").post(addComment).get(getVideoComments);
+router.route("/c/:commentId").patch(updateComment).delete(deleteComment);
 
 export default router;
