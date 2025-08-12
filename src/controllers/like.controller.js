d9ca2eb2556192db62e231607215e73a18a2bcdf
sanjiv <sanjivbatajoo@gmail.com>
2025-08-12 14:55:15 -0500

    add new feature: togggleTweetLike, toggleVideoLike, toggleCommentLike and getLikedVideos in like controller and updated like routes

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -26,9 +26,7 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
             likedBy: req.user._id,
         });
     } else {
-        newLike = await Like.findByIdAndDelete(like._id, {
-            video: 1,
-        });
+        newLike = await Like.findByIdAndDelete(like._id);
     }
 
     if (!newLike) {
@@ -48,4 +46,127 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         );
 });
 
-export {toggleVideoLike};
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params;
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Error: Invalid Comment Id");
+    }
+
+    if (!req.user?._id) {
+        throw new ApiError(400, "Error: User not authorized");
+    }
+
+    const like = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id,
+    });
+
+    let newLike;
+    if (!like) {
+        newLike = await Like.create({
+            likedBy: req.user?._id,
+            comment: commentId,
+        });
+    } else {
+        newLike = await Like.findByIdAndDelete(like?._id);
+    }
+
+    if (!newLike) {
+        throw new ApiError(400, "Error: Failed to update Like object");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                newLike,
+                like
+                    ? "Successful unliking the comment"
+                    : "Successful liking the comment"
+            )
+        );
+});
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params;
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Error: Invalid Comment Id");
+    }
+
+    if (!req.user?._id) {
+        throw new ApiError(400, "Error: User not authorized");
+    }
+
+    const like = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id,
+    });
+
+    let newLike;
+    if (!like) {
+        newLike = await Like.create({
+            likedBy: req.user?._id,
+            tweet: tweetId,
+        });
+    } else {
+        newLike = await Like.findByIdAndDelete(like?._id);
+    }
+
+    if (!newLike) {
+        throw new ApiError(400, "Error: Failed to update Like object");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                newLike,
+                like
+                    ? "Successful unliking the tweet"
+                    : "Successful liking the tweet"
+            )
+        );
+});
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    const user = req.user?._id;
+    if (!user) {
+        throw new ApiError(400, "Unauth: User not authorized");
+    }
+
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: user,
+                video: {
+                    $exists: true,
+                },
+            },
+        },
+        {
+            $project: {
+                video: 1,
+            },
+        },
+    ]);
+
+    if (!likedVideos) {
+        throw new ApiError(400, "Error: Failed to get data from db");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                likedVideos,
+                "Successfully fetched liked videos"
+            )
+        );
+});
+
+export {toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos};
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,11 +1,19 @@
 import {Router} from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
-import {toggleVideoLike} from "../controllers/like.controller.js";
+import {
+    getLikedVideos,
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+} from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/video/:videoId").post(toggleVideoLike);
+router.route("/comment/:commentId").post(toggleCommentLike);
+router.route("/tweet/:tweetId").post(toggleTweetLike);
+router.route("/video").get(getLikedVideos);
 
 export default router;
